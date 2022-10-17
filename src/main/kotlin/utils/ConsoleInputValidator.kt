e8bbec3347c8a5c1fe5fa2f3d748fca6440c2852
Alka nixon <20100677@mail.wit.ie>
2022-10-17 13:26:56 +0100

    Lab1

---
+++ src/main/kotlin/utils/ConsoleInputValidator.kt
@@ -0,0 +1,13 @@
+package utils
+
+fun readValidEmail(): String {
+    var email = ""
+    do {
+        print("    Email: ")
+        email = readLine()!!
+        when {
+            isValidEmail(email) -> return email
+            else -> print ("       Invalid Email format.  Try again.")
+        }
+    } while (true)
+}
