c67f25a1c095e15174936c073142ac96b0ba1d15
Alka nixon <20100677@mail.wit.ie>
2022-10-17 21:54:45 +0100

    Lab2 - exercises

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import utils.readValidEmail
 import utils.validGender
 import controllers.UserStore
 import mu.KotlinLogging
+import utils.Conversion
 
 val userStore = UserStore()
 private val logger = KotlinLogging.logger {}
@@ -11,6 +12,13 @@ private val logger = KotlinLogging.logger {}
 fun main(){
     logger.info{"Welcome to Health Tracker"}
     logger.info {"Health Tracker App has started"}
+    //Some Temporary Test Data
+    userStore.create((User(1, "Homer Simpson", "homer@simpson.com", 178.0, 2.0f, 'M')))
+    userStore.create((User(2, "Marge Simpson", "marge@simpson.com", 140.0, 1.6f, 'F')))
+    userStore.create((User(3, "Lisa Simpson", "lisa@simpson.com", 100.0, 1.2f, 'F')))
+    userStore.create((User(4, "Bart Simpson", "bart@simpson.com", 80.0, 1.0f, 'M')))
+    userStore.create((User(5, "Maggie Simpson", "maggie@simpson.com", 50.0, 0.7f, 'F')))
+
     runApp()
 }
 
@@ -24,7 +32,10 @@ fun runApp(){
             3 -> searchById()
             4 -> deleteUser()
             5 -> updateUser()
-            in(6..10) -> println("Feature coming soon")
+            6 -> searchByGender()
+            7 -> usersReport()
+            8 -> usersImperial()
+            in(9..10) -> println("Feature coming soon")
             0 -> println("Bye...")
             else -> print("Invalid Option")
         }
@@ -39,9 +50,34 @@ fun menu(): Int{
         |  3. Search by Id
         |  4. Delete by Id
         |  5. Update by Id
+        |  6. Search by Gender
+        |  7. Users Report
+        |  8. Users (imperial)
         |  0. Exit
         |Please enter your option: """.trimMargin())
     return readLine()?.toIntOrNull() ?: -1
+}
+
+fun usersReport() {
+
+    val users = userStore.findAll()
+
+    println("""
+        |------------------------
+        |     USERS REPORT
+        |------------------------
+        |
+        |  Number of Users:  ${users.size}
+        |  Gender Breakdown: ${users.groupingBy{it.gender}.eachCount()}
+        |  Average Weight:   ${users.sumOf { it.weight }.div(users.size)} kg
+        |  Min Weight:       ${users.minByOrNull { it.weight }?.weight} kg
+        |  Max Weight:       ${users.maxByOrNull { it.weight }?.weight} kg
+        |  Average Height:   ${Conversion.round(users.sumOf { it.height.toDouble() }.div(users.size), 2.0)} metres
+        |  Min Height:       ${users.minByOrNull { it.height }?.height} metres
+        |  Max Height:       ${users.maxByOrNull { it.height }?.height} metres
+        |
+        |------------------------
+        |""".trimMargin())
 }
 
 fun addUser(){
@@ -79,7 +115,27 @@ fun updateUser() {
 }
 fun getUserById() : User?{
     print("Enter the id of the user: ")
-    return  userStore.findOne(readLine()?.toIntOrNull() ?: -1)
+    return userStore.findOne(readLine()?.toIntOrNull() ?: -1)
+}
+
+fun searchByGender() {
+    print("Search by Gender(M,F,O): ")
+    val gender = validGender(readLine()!!.getOrNull(0) ?: ' ')
+    userStore.findAll()
+        .filter { it.gender.equals(gender, ignoreCase = true) }
+        .sortedBy { it.name }
+        .forEach{println(it)}
+}
+
+fun usersImperial() {
+    println("The user details (imperial) are:")
+    userStore.findAll()
+        .sortedBy { it.name }
+        .forEach{println("  " + it.name
+                + "(" + it.email + "), "
+                + Conversion.convertKGtoPounds(it.weight, 1.0) + " pounds, "
+                + Conversion.convertMetresToInches(it.height.toDouble(), 1.0) + " inches."
+        )}
 }
 
 fun searchById() {
